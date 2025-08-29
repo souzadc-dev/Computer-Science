5ff5c109438f6a23c7bc5b3d0d4cc987d8e43949
souzadc-dev <souzadc-dev@gmail.com>
2025-08-29 18:31:20 -0300

    done data structure array

--- Estrutura de dados/Java/src/Vetor.java
+++ Estrutura de dados/Java/src/Vetor.java
@@ -2,19 +2,23 @@ import java.util.Arrays;
 
 public class Vetor {
 
-    private static final int TAMANHO = 10_000_000;
+    private static final int TAMANHO = 5;
 
     public static void main(String[] args) {
         long[] array = new long[TAMANHO];
         long startTime = System.currentTimeMillis();
 
+        // O(n²)
         for (int i = 0; i < TAMANHO; i++) {
-            array[i] = i;
+            for (int j = 0; j < TAMANHO; j++) {
+                array[i] += j;
+            }
         }
 
         long endTime = System.currentTimeMillis();
 
         System.out.println("Tempo de execução: "+ (endTime - startTime)+ " ms.");
-       // System.out.println(Arrays.toString(array));
     }
 }
+
+// 22:22
